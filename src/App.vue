544bd5dc5ef430bd30f216c62e7e5c0c1f75e0c2
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-13 15:42:47 +0100

    import Yu-Gi-Oh Api with axios

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,31 @@
 </template>
 
 <script>
+import { store } from './data/store.js';
 import axios from "axios";
 import CardComponent from './components/CardComponent.vue';
 import TitleComponent from './components/TitleComponent.vue';
 
 export default {
   name: 'App',
-  components: { TitleComponent, CardComponent }
+  components: { TitleComponent, CardComponent },
+  data() {
+    return {
+      store,
+      cardList: [],
+    }
+  },
+  methods: {
+    getCard() {
+      axios.get(store.apiUrl).then((card) => {
+        this.cardList.push(card.data)
+      })
+    }
+  },
+  created() {
+    this.getCard();
+    console.log(this.cardList);
+  },
 }
 </script>
 
