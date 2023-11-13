64274117b885d45371fc975c594b1d72ebf60625
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-13 17:33:38 +0100

    added transition to card background

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="card">
+    <div class="card p-2 text-white">
         <img :src="image" class="card-img-top" :alt="name">
         <div class="card-body">
             <h5 class="card-title ">{{ name }}</h5>
-            <h6>{{ species }}</h6>
         </div>
+        <h6 class="mx-3">{{ species }}</h6>
     </div>
 </template>
 
@@ -20,7 +20,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.card {
+    background: linear-gradient(90deg, rgba(84, 11, 185, 1) 0%, rgba(77, 128, 189, 1) 90%);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 0%;
+    }
+
+    50% {
+        background-position: 100% 100%;
+    }
+
+    100% {
+        background-position: 0% 0%;
+    }
+}
+
 .card-body {
-    height: 100px;
+    height: 75px;
 }
 </style>
